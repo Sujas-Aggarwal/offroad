98c40356e5e741f695749c970fbb02eb00c07169
Sujas Aggarwal <ce1221047@iitd.ac.in>
2024-05-23 11:53:02 +0530

    Erro page

--- src/App.jsx
+++ src/App.jsx
@@ -28,14 +28,23 @@ function App() {
         {
             path: "/analytics/:id",
             element: <Analytics />,
-        },{
+        },
+        {
             path: "/link/:id",
-            element: <Redirect/>,
-        }
+            element: <Redirect />,
+        },
+        {
+            path: "*",
+            element: (
+                <div className="w-full h-[90vh] justify-center items-center flex text-4xl text-white font-bold">
+                    Page Not Found
+                </div>
+            ),
+        },
     ]);
     return (
         <div className="w-full max-w-[100%] overflow-x-hidden h-full min-h-screen bg-[#ff9a00]">
-            <Toaster/>
+            <Toaster />
             <RouterProvider router={router} />
             <Footer />
         </div>
